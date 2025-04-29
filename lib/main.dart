c81a9236f203b80a64970febc16f0f480cc357d2
Juan Dominici <juancdominici@gmail.com>
2025-04-28 21:33:58 -0300

    Implement connectivity handling

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_offline/flutter_offline.dart';
 import 'package:sumit/theme.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
@@ -8,6 +9,7 @@ import 'package:june/june.dart';
 import 'package:sumit/state/settings.dart';
 import 'package:sumit/utils.dart';
 import 'package:posthog_flutter/posthog_flutter.dart';
+import 'package:sumit/utils/translations_extension.dart';
 import 'router.dart';
 
 Future<void> main() async {
@@ -74,42 +76,98 @@ class _MyAppState extends State<MyApp> with WidgetsBindingObserver {
 
   @override
   Widget build(BuildContext context) {
-    return JuneBuilder(
-      () => SettingsState(),
-      builder: (settingsState) {
-        if (settingsState.isLoading) {
-          return MaterialApp(
-            debugShowCheckedModeBanner: false,
-            theme: lightTheme,
-            darkTheme: darkTheme,
-            home: Scaffold(body: Center(child: CircularProgressIndicator())),
+    return OfflineBuilder(
+      connectivityBuilder: (
+        BuildContext context,
+        List<ConnectivityResult> connectivity,
+        Widget child,
+      ) {
+        final bool connected = !connectivity.contains(ConnectivityResult.none);
+
+        if (connected) {
+          return JuneBuilder(
+            () => SettingsState(),
+            builder: (settingsState) {
+              if (settingsState.isLoading) {
+                return MaterialApp(
+                  debugShowCheckedModeBanner: false,
+                  theme: lightTheme,
+                  darkTheme: darkTheme,
+                  home: Scaffold(
+                    body: Center(child: CircularProgressIndicator()),
+                  ),
+                );
+              }
+
+              return JuneBuilder(
+                () => TranslationsService(),
+                builder: (translationsService) {
+                  return MaterialApp.router(
+                    theme: lightTheme,
+                    darkTheme: darkTheme,
+                    themeMode:
+                        settingsState.userPreferences.darkMode
+                            ? ThemeMode.dark
+                            : ThemeMode.light,
+                    title: translationsService.translate('app.title'),
+                    debugShowCheckedModeBanner: false,
+                    routerConfig: appRouter,
+                    localizationsDelegates: const [
+                      GlobalMaterialLocalizations.delegate,
+                      GlobalWidgetsLocalizations.delegate,
+                      GlobalCupertinoLocalizations.delegate,
+                    ],
+                    supportedLocales: const [Locale('en'), Locale('es')],
+                    locale: translationsService.currentLocale,
+                  );
+                },
+              );
+            },
           );
         }
-
-        return JuneBuilder(
-          () => TranslationsService(),
-          builder: (translationsService) {
-            return MaterialApp.router(
-              theme: lightTheme,
-              darkTheme: darkTheme,
-              themeMode:
-                  settingsState.userPreferences.darkMode
-                      ? ThemeMode.dark
-                      : ThemeMode.light,
-              title: translationsService.translate('app.title'),
-              debugShowCheckedModeBanner: false,
-              routerConfig: appRouter,
-              localizationsDelegates: const [
-                GlobalMaterialLocalizations.delegate,
-                GlobalWidgetsLocalizations.delegate,
-                GlobalCupertinoLocalizations.delegate,
-              ],
-              supportedLocales: const [Locale('en'), Locale('es')],
-              locale: translationsService.currentLocale,
-            );
-          },
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          theme: lightTheme,
+          darkTheme: darkTheme,
+          home: Scaffold(
+            body: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Icon(
+                    Icons.cloud_off_rounded,
+                    size: 90,
+                    color: Theme.of(context).disabledColor,
+                  ),
+                  const SizedBox(height: 16),
+                  Text(
+                    context.translate('offline.message'),
+                    style: TextStyle(fontSize: 18),
+                  ),
+                  const SizedBox(height: 8),
+                  Text(
+                    context.translate('offline.description'),
+                    style: TextStyle(fontSize: 16),
+                  ),
+                ],
+              ),
+            ),
+          ),
         );
       },
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Icon(
+            Icons.cloud_off_rounded,
+            size: 64,
+            color: Theme.of(context).colorScheme.secondary,
+          ),
+          const SizedBox(height: 16),
+          Text(context.translate('offline.message')),
+          Text(context.translate('offline.description')),
+        ],
+      ),
     );
   }
 }
